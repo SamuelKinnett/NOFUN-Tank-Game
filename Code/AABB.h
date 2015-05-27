d70d2775b3295e92d630e00d318fe6f25e2329f7
Samuel Kinnett <samuelkinnett@member.fsf.org>
2015-05-27 22:31:13 +0100

    Split tank class, began work on collision detection
    Split the tank class into a .cpp and .h file to allow for integration in
    other parts of the program. Also began implementing an AABB class to
    allow for basic collision detection.

---
+++ Code/AABB.h
@@ -0,0 +1,4 @@
+//A class describing an axis aligned bounding box
+class AABB {
+	
+}
--- Code/tank.cpp
+++ Code/tank.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <tank.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -11,39 +12,6 @@
 #define SIDES 1
 #define REAR 2
 #define DEGREES_TO_RADIANS 0.0174532925f
-
-//A class describing a tank
-class Tank {
-   //General Information
-   float hullFacing, turretFacing, hullTraverseRate, turretTraverseRate;
-   //The angle that the hull and turret are facing, in radians, and the
-   //rate at which both rotate in radians per tick
-   double positionX, positionY;
-   //The X and Y co-ordinates of the tank
-   int hp, maxHP;
-   //The health points that the tank has currently and the absolute maximum
-   //health
-   float armour [3];
-   //The armour of the tank, in the format front, sides, rear
-
-   //Weapon Information
-   int currentAmmo [3], maxAmmo [3], damage [3], penetration [3];
-   //Information about the tank's ammo, in the format AP, APCR, HE
-
-   //Engine/Movement Information
-   int moving;
-   //Is the tank currently moving? 0 = stationary, 1 = moving
-   float horsepower, weight, currentSpeed, topSpeed;
-   //The horsepower of the engine, the weight of the tank, its current speed
-   //and its top speed
-
-   public:
-      void Move(int);
-      void Traverse(int);
-      void Update();
-      void RotateTurret(int);
-      void Hit(float, int, int, int);
-};
 
 //This method handles moving the tank, given a direction
 //2 = forwards, 1 = backwards, 0 = no input
