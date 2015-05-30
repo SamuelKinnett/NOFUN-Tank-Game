d4a9a7ba4507b0ff54f16b8abb0335cc9c9c2f85
p512 <point6000x+gh@gmail.com>
2015-05-31 00:44:05 +0200

    Restructured tank.h

--- Code/include/tank.h
+++ Code/include/tank.h
@@ -10,35 +10,36 @@
 //==============================================================================
 //Class definition
 class Tank {
-   //General Information
-   float hullFacing, turretFacing, hullTraverseRate, turretTraverseRate;
-   //The angle that the hull and turret are facing, in radians, and the
-   //rate at which both rotate in radians per tick
-   double positionX, positionY;
-   //The X and Y co-ordinates of the tank
-   int hp, maxHP;
-   //The health points that the tank has currently and the absolute maximum
-   //health
-   float armour [3];
-   //The armour of the tank, in the format front, sides, rear
+    public:
+        void Move(int, float);
+        void Traverse(int);
+        void Update(float);
+        void RotateTurret(int);
+        void Hit(float, int, int, int);
 
-   //Weapon Information
-   int currentAmmo [3], maxAmmo [3], damage [3], penetration [3];
-   //Information about the tank's ammo, in the format AP, APCR, HE
+    private:
+        //General Information
+        float hullFacing, turretFacing, hullTraverseRate, turretTraverseRate;
+        //The angle that the hull and turret are facing, in radians, and the
+        //rate at which both rotate in radians per tick
+        double positionX, positionY;
+        //The X and Y co-ordinates of the tank
+        int hp, maxHP;
+        //The health points that the tank has currently and the absolute maximum
+        //health
+        float armour [3];
+        //The armour of the tank, in the format front, sides, rear
 
-   //Engine/Movement Information
-   bool moving;
-   //Is the tank currently moving?
-   float horsepower, weight, currentSpeed, topSpeed;
-   //The horsepower of the engine, the weight of the tank, its current speed
-   //and its top speed
+        //Weapon Information
+        int currentAmmo [3], maxAmmo [3], damage [3], penetration [3];
+        //Information about the tank's ammo, in the format AP, APCR, HE
 
-   public:
-      void Move(int, float);
-      void Traverse(int);
-      void Update(float);
-      void RotateTurret(int);
-      void Hit(float, int, int, int);
+        //Engine/Movement Information
+        bool moving;
+        //Is the tank currently moving?
+        float horsepower, weight, currentSpeed, topSpeed;
+        //The horsepower of the engine, the weight of the tank, its current speed
+        //and its top speed
 };
 
 #endif
