b8124731600bf29b7d94048940e8c472b6652fda
p512 <point6000x+gh@gmail.com>
2015-06-07 17:26:07 +0200

    Added collisionGeneration.hpp

---
+++ Code/include/collisionGeneration.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef _COLLISIONGENERATION_HPP_INCLUDED_
+#define _COLLISIONGENERATION_HPP_INCLUDED_
+
+/*this header file will include functions that create an edge line from a 
+  tank texture which can then in turn be used for fairly easy collision detection*/
+
+#endif
