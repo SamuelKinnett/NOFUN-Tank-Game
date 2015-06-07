664ea3f560895e91bb95fbc3e94fe2815c1a9b58
p512 <point6000x+gh@gmail.com>
2015-06-07 20:39:15 +0200

    Added Skeleton for Modules and Engine

---
+++ Code/include/Engine.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _ENGINE_HPP_INCLUDED_
+#define _ENGINE_HPP_INCLUDED_
+
+#include "Module.hpp"
+
+class Engine : public Module
+{
+    public:
+        virtual void step(double deltaTime)
+        {
+            
+        }
+};
+
+#endif
---
+++ Code/include/Module.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#ifndef _MODULE_HPP_INCLUDED_
+#define _MODULE_HPP_INCLUDED_
+
+class Module
+{
+    public:
+        virtual void step(double deltaTime) = 0;
+
+        //enum describing if a module is damaged
+        enum damage 
+        {
+            USABLE,
+            DAMAGED,
+            BROKEN
+        };
+        damage getDamageState()
+        {
+            return damageState;
+        }
+        double getHP()
+        {
+            return HP;
+        }
+        double getMaxHP()
+        {
+            return maxHP;
+        }
+        void hit(double pen, double dmg)
+        {
+            if (pen >= penDurability)
+            {
+                HP -= dmg;
+            }
+            if (HP < 0) {
+                HP = 0;
+                damageState = BROKEN;
+            }
+            else if (HP < maxHP / 2)
+            {
+                damageState = DAMAGED;
+            }
+        }
+    private:
+        damage damageState;
+        double HP;
+        double maxHP;
+        //penetration needed to damage the module
+        double penDurability;
+};
+
+#endif
