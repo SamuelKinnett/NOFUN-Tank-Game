d13dba5cadf51ecc26d6094d575aa56813d18829
p512 <point6000x+gh@gmail.com>
2015-06-08 22:54:59 +0200

    Added Track skeleton

---
+++ Code/include/Track.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _TRACK_HPP_INCLUDED_
+#define _TRACK_HPP_INCLUDED_
+
+#include "Module.hpp"
+
+class Track : public Module
+{
+    public:
+        virtual void update(double deltaTime)
+        {
+
+        }
+};
+
+#endif
